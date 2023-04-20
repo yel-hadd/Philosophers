f5456691a10a3d2b0a49a98256596a21fb3577be
Yassine El Haddad <yel-hadd@mail.com>
2023-04-20 22:12:52 +0000

    a

--- f.c
+++ f.c
@@ -1,48 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   f.c                                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:23:01 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/20 21:14:12 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/20 20:53:53 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#define BIG 1000000000UL
-uint32_t	counter = 0;
+
+void *my_turn(void *arg)
+{
+	int	i;
+	int	*ptr = (int *) arg;
+
+	i = -1;
+	while(++i < 10)
+	{
+		*ptr = i;
+		printf("%d\n", *ptr);
+		sleep(1);
+	}
+	return (ft_strdup("Hola\n"));
+}
+
+void *your_turn()
+{
+	int	i;
+
+	i = -1;
+	while(++i < 2)
+	{
+		printf("your turn\n");
+		sleep(2);
+	}
+	return (NULL);
+}
+
 //char	*set_str()
 //{
 //	
 //}
 
-void	*incr_to_big_1(void *arg)
-{
-	uint32_t i = -1;
-	pthread_mutex_t *lock;
-
-	lock = (pthread_mutex_t *) arg;
-
-	while(++i < BIG)
-	{
-		pthread_mutex_lock(lock);
-		counter++;
-		pthread_mutex_unlock(lock);
-	}
-	return (NULL);
-}
-
 int	main(void)
 {
-	pthread_t	t;
-	pthread_mutex_t	lock = PTHREAD_MUTEX_INITIALIZER;
-	//int	i;
-	//char	*str;
+	pthread_t new;
+	int	i;
+	char	*str;
 
-	pthread_create(&t, NULL, incr_to_big_1, &lock);
-	incr_to_big_1((void *) &lock);
-	pthread_join(t, NULL);
-	printf("---* %d\n", counter);
+	i = 0;
+	pthread_create(&new, NULL, my_turn, &i);
+	your_turn();
+	pthread_join(new, (void *) &str);
+	printf("---* %s\n", str);
+	printf("---* %d\n", i);
 }
