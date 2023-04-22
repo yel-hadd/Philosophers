9ba2b7b1f216fc127c4479e563fbac9665f3fec6
Yassine El Haddad <yel-hadd@mail.com>
2023-04-22 23:06:19 +0000

    Update new_fork.c

--- utils/new_fork.c
+++ utils/new_fork.c
@@ -6,13 +6,13 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:54:34 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/22 15:41:30 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/22 20:45:26 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-t_fork	*init_forks(int id, int taken)
+t_fork	*new_fork(int id, int taken)
 {
 	t_fork	*node;
 
