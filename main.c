57f31433afb1b810c0cacaab5c02da4e8ab56b81
Yassine El Haddad <yel-hadd@mail.com>
2023-04-22 17:19:41 +0000

    Update main.c

--- main.c
+++ main.c
@@ -5,44 +5,75 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/18 21:23:01 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/20 21:14:12 by yel-hadd         ###   ########.fr       */
+/*   Created: 2023/04/21 18:24:45 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/04/22 17:16:30 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#define BIG 1000000000UL
-uint32_t	counter = 0;
-//char	*set_str()
-//{
-//	
-//}
 
-void	*incr_to_big_1(void *arg)
+void	fork_add_back(t_fork **lst, t_fork *new)
 {
-	uint32_t i = -1;
-	pthread_mutex_t *lock;
+	t_fork	*temp;
 
-	lock = (pthread_mutex_t *) arg;
-
-	while(++i < BIG)
+	temp = *lst;
+	if (!temp)
+		*lst = new;
+	else
 	{
-		pthread_mutex_lock(lock);
-		counter++;
-		pthread_mutex_unlock(lock);
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
 	}
-	return (NULL);
 }
 
-int	main(void)
-{
-	pthread_t	t;
-	pthread_mutex_t	lock = PTHREAD_MUTEX_INITIALIZER;
-	//int	i;
-	//char	*str;
+//t_fork  *parse_forks(int num, char **arg)
+//{
+//	int		i;
+//	t_fork	*f;
+//	
+//
+//	i = 0;
+//
+//	while (i)
+//	{
+//		fork_add_back(&f, new_fork(ft_atoi(arg[1]), 0));
+//	}
+//}
 
-	pthread_create(&t, NULL, incr_to_big_1, &lock);
-	incr_to_big_1((void *) &lock);
-	pthread_join(t, NULL);
-	printf("---* %d\n", counter);
+int	check_params(t_num *no)
+{
+	if (no->ttd == -1 || no->tte == -1)
+		return (0);
+	if (no->tts == -1 || no->n_eat == -1)
+		return (0);
+	return (1);
+}
+
+t_num	*parse_params(char **arg, int arg_count)
+{
+	t_num	*node;
+
+	node = (t_num *)malloc(sizeof(t_num));
+	if (!node)
+		return (node);
+	node->ttd = ft_atoi(arg[1]);
+	node->tte = ft_atoi(arg[2]);
+	node->tts = ft_atoi(arg[3]);
+	node->n_eat = -2;
+	if (arg_count == 5)
+		node->n_eat = ft_atoi(arg[4]);
+	node->next = NULL;
+	return (node);
+}
+
+int main(int ac, char **av)
+{
+	t_num	*n;
+
+	n = parse_params(av, ac);
+	if (!check_params(n))
+		return (1);
+	printf("%d\n", n->n_eat);
+	return (0);
 }
