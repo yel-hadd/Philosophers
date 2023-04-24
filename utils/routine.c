f909ca7b267701717560220608e685eeabd9d690
Yassine El Haddad <yel-hadd@mail.com>
2023-04-24 22:26:11 +0100

    Update routine.c

--- utils/routine.c
+++ utils/routine.c
@@ -6,31 +6,30 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 19:31:44 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/24 19:52:09 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/24 21:23:14 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
+/*
+timestamp_in_ms X has taken a fork
+timestamp_in_ms X is eating
+timestamp_in_ms X is sleeping
+timestamp_in_ms X is thinking
+timestamp_in_ms X died
+*/
+
 void    *routine(void *ptr)
 {
 	struct timeval	t;
-	t_num			*m;
+	t_philo			*m;
 
-	m = (t_num *) ptr;
-	while (1)
-	{
-		pthread_mutex_lock(&m->current->lf->lock);
-		pthread_mutex_lock(&m->current->rf->lock);
-		pthread_mutex_lock(&m->current->lock);
-		gettimeofday(&t, NULL);
-		printf("%d %d has taken a fork", m->ms - t.tv_usec, m->current->id);
-		gettimeofday(&t, NULL);
-		printf("%d %d is eating", m->ms - t.tv_usec, m->current->id);
-		m->current->last_eat = m->ms - t.tv_usec;
-		usleep(100);
-		pthread_mutex_unlock(&m->current->lf->lock);
-		pthread_mutex_unlock(&m->current->rf->lock);
-		pthread_mutex_unlock(&m->current->lock);
-	}
+	m = (t_philo *) ptr;
+	// THINK
+	// PICK UP FORK
+	// EAT
+	// PUT DOWN FORK
+	// SLEEP
+	// CHECK IF PHILO DIED
 }
