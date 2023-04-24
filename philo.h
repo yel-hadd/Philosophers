4b09b779019a3745b30d51b0744d856e4fdc6e33
Yassine El Haddad <yel-hadd@mail.com>
2023-04-24 21:16:16 +0100

    Update philo.h

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:24:02 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/24 18:43:48 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/24 20:16:03 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <pthread.h>
-# include <sys/time.h>;
+# include <sys/time.h>
 #include <stdio.h>
 
 struct timeval tv;
+
+typedef __darwin_suseconds_t msecond;
 
 typedef struct forks
 {
@@ -33,6 +35,13 @@ typedef struct forks
 typedef struct philosopher
 {
 	int					id;
+	int					ttd;
+	int 				tte;
+	int					tts;
+	long				ms;
+	int					n_eat;
+	int					max_eat;
+	long				last_eat;
 	t_fork 				*lf;
 	t_fork 				*rf;
 	pthread_mutex_t		lock;
@@ -43,31 +52,33 @@ typedef struct num
 {
 	int			n_phil;
 	int			ttd;
+	int			n_eat;
 	int 		tte;
 	int			tts;
-	int			n_eat;
+	long		ms;
 	t_philo		*p;
+	t_philo		*current;
 	t_fork		*f;
 	struct num	*next;
 } t_num;
 
+t_num	*parse_params(char **arg, int arg_count, long time);
+t_philo	*new_philo(int id, t_fork *r, t_fork *l, int max);
+void	parse_philos(t_num *n, t_fork **f, int n_phil);
+void	philo_add_back(t_philo **lst, t_philo *new);
 void	*ft_memcpy(void *dst, void *src, size_t n);
-t_philo	*new_philo(int id, t_fork *r, t_fork *l);
 int		ft_strncmp(char *s1, char *s2, size_t n);
+void	fork_add_back(t_fork **lst, t_fork *new);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_calloc(size_t count, size_t size);
+void	parse_forks(t_fork **f, int n_phil);
 t_fork	*new_fork(int id, int taken);
 void	ft_bzero(void *s, size_t n);
+int		check_params(t_num *no);
 char	*ft_strdup(char *s1);
 int		ft_atoi(char *str);
 size_t	ft_strlen(char *s);
 int		ft_isdigit(int c);
-void	philo_add_back(t_philo **lst, t_philo *new);
-t_num	*parse_params(char **arg, int arg_count);
-void	parse_forks(t_fork **f, int n_phil);
-void	fork_add_back(t_fork **lst, t_fork *new);
-int		check_params(t_num *no);
-void	parse_philos(t_num *n, t_fork **f, int n_phil);
 
 
 #endif
