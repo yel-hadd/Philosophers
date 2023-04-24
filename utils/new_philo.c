7c621294373fb2ea89eda2ac66ee183752ca3c72
Yassine El Haddad <yel-hadd@mail.com>
2023-04-24 22:26:07 +0100

    Update new_philo.c

--- utils/new_philo.c
+++ utils/new_philo.c
@@ -6,13 +6,13 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:54:29 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/24 19:44:32 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/24 21:16:08 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-t_philo	*new_philo(int id, t_fork *r, t_fork *l, int max)
+t_philo	*new_philo(t_num *n, int id, t_fork *r, t_fork *l)
 {
 	t_philo	*node;
 
@@ -23,9 +23,11 @@ t_philo	*new_philo(int id, t_fork *r, t_fork *l, int max)
 	node->lf = l;
 	node->rf = r;
 	node->last_eat = 0;
-	node->n_eat = 0;
-	node->max_eat = max;
-	pthread_mutex_init(&node->lock, NULL);
+	node->n_meals = 0;
+	node->max_eat = n->n_eat;
+	node->ttd = n->ttd;
+	node->tts = n->tts;
+	node->tte = n->tte;
 	node->next = NULL;
 	return (node);
 }
