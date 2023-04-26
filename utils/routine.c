7f87db72193985a19be136fcc1380a1f71e7d7ea
Yassine El Haddad <yel-hadd@mail.com>
2023-04-26 15:28:30 +0100

    Update routine.c

--- utils/routine.c
+++ utils/routine.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 19:31:44 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/24 21:23:14 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/26 14:28:09 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,25 @@
 
 /*
 timestamp_in_ms X has taken a fork
-timestamp_in_ms X is eating
+
 timestamp_in_ms X is sleeping
 timestamp_in_ms X is thinking
 timestamp_in_ms X died
+9160
 */
+
+long get_ms_ts(long long subtract)
+{
+	struct timeval	tv;
+	long		time_stamp;
+
+	gettimeofday(&tv, NULL);
+	time_stamp = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	if (subtract > 0)
+		time_stamp -= subtract;
+	return (time_stamp);
+}
+
 
 void    *routine(void *ptr)
 {
@@ -26,10 +40,21 @@ void    *routine(void *ptr)
 	t_philo			*m;
 
 	m = (t_philo *) ptr;
-	// THINK
-	// PICK UP FORK
+
+	// PICK UP LEFT FORK
+	pthread_mutex_lock(&m->lf->lock);
+	// PICK UP RIGHT FORK
+	pthread_mutex_lock(&m->rf->lock);
 	// EAT
-	// PUT DOWN FORK
+	printf("%ld %d is eating", get_ms_ts(m->id));
+	m->n_meals += 1;
+	usleep(m->tte);
+	// PUT DOWN LEFT FORK
+	pthread_mutex_unlock(&m->lf->lock);
+	// PUT DOWN RIGHT FORK
+	pthread_mutex_unlock(&m->rf->lock);
 	// SLEEP
+	usleep(m->tts);
 	// CHECK IF PHILO DIED
+	// THINK
 }
