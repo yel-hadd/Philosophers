7f82aeca113e993c377bbeadf931b5b5909f4f66
Yassine El Haddad <yel-hadd@mail.com>
2023-04-28 21:07:31 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 18:24:45 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/27 21:25:18 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/28 19:53:55 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	main(int ac, char **av)
 	t_num	*n;
 	t_fork	*fork;
 	long	time;
+	t_philo	*tmp;
 
 	time = get_ms_ts(0);
 	n = NULL;
@@ -34,6 +35,18 @@ int	main(int ac, char **av)
 		return (1);
 	parse_forks(&n->f, n->n_phil);
 	parse_philos(n, &n->f, n->n_phil);
+	tmp = n->p;
+	while (tmp != NULL)
+	{
+		pthread_create(tmp->thrd, NULL, routine, &tmp);
+		tmp = tmp->next;
+	}
+	tmp = n->p;
+	while (tmp != NULL)
+	{
+		pthread_join(*tmp->thrd, NULL);
+		tmp = tmp->next;
+	}
 	return (0);
 }
 
