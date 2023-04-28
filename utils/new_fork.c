ce641e8c4e2bfcdaac61591c3f8fbb2ab45f6107
Yassine El Haddad <yel-hadd@mail.com>
2023-04-28 21:07:42 +0100

    Update new_fork.c

--- utils/new_fork.c
+++ utils/new_fork.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:54:34 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/23 19:06:41 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/28 14:15:03 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@ t_fork	*new_fork(int id, int taken)
 {
 	t_fork	*node;
 
+	pthread_mutex_t *lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	if (lock == NULL)
+	    return NULL;
 	node = (t_fork *)malloc(sizeof(t_fork));
+	node->lock = lock;
+	pthread_mutex_init(node->lock, NULL);
 	if (!node)
 		return (node);
 	node->id = id;
     node->taken = taken;
-	pthread_mutex_init(&node->lock, NULL);
 	node->next = NULL;
 	return (node);
 }
