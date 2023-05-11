4c0f1dafa70708a8979385cb6f1cb8427717648e
Yassine El Haddad <yel-hadd@mail.com>
2023-05-11 18:37:56 +0100

    Update ft_strdup.c

--- utils/ft_strdup.c
+++ utils/ft_strdup.c
@@ -21,6 +21,6 @@ char	*ft_strdup(char *s1)
 	s2 = ft_calloc((len + 1), sizeof(char));
 	if (!s2)
 		return (0);
-	ft_memcpy(s2, (void *) s1, ((len + 1) * sizeof(char)));
+	ft_memcpy(s2, (void *)s1, ((len + 1) * sizeof(char)));
 	return (s2);
 }
