0121fd249c88e58db453e154144bf22cb1b0e520
Yassine El Haddad <yel-hadd@mail.com>
2023-05-11 17:09:27 +0100

    Revert "fixed infinite loop"
    This reverts commit 0aef3cc55d4d00ca4e97c6a47419fcf10761d379.

---
+++ main.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 18:24:45 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/05/11 17:09:25 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int ac, char **av)
+{
+	t_num	*n;
+	t_fork	*fork;
+	long	time;
+	t_philo	*tmp;
+
+	time = get_ms_ts(0);
+	n = NULL;
+	fork = NULL;
+	n = parse_params(av, ac, time);
+	if (!check_params(n))
+		return (1);
+	parse_forks(&n->f, n->n_phil);
+	parse_philos(n, &n->f, n->n_phil);
+	tmp = n->p;
+	while (tmp != NULL)
+	{
+		pthread_create(tmp->thrd, NULL, routine, tmp);
+		tmp = tmp->next;
+	}
+	tmp = n->p;
+	while (tmp != NULL)
+	{
+		pthread_join(*tmp->thrd, NULL);
+		tmp = tmp->next;
+	}
+	while (1);
+	
+	
+	return (0);
+}
