eded826cefc2ae927d05df9523a14be5197115f1
Yassine El Haddad <yel-hadd@mail.com>
2023-05-11 18:37:33 +0100

    Update mk.sh

--- mk.sh
+++ mk.sh
@@ -1,2 +1,3 @@
-#gcc -fsanitize=thread -Wall -Werror -Wextra -g main.c utils/* -o philo
+#gcc - fsanitize = thread - Wall - Werror - Wextra - g main.c utils/* -o philo
+
 gcc -Wall -Werror -Wextra -g main.c utils/* -o philo
