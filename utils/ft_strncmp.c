4b7c54f86bb7d78557877359f07283f5bbc322aa
Yassine El Haddad <yel-hadd@mail.com>
2023-05-11 18:38:01 +0100

    Update ft_strncmp.c

--- utils/ft_strncmp.c
+++ utils/ft_strncmp.c
@@ -18,14 +18,14 @@ int	ft_strncmp(char *s1, char *s2, size_t n)
 	unsigned char	*uc1;
 	unsigned char	*uc2;
 
-	uc1 = (unsigned char *) s1;
-	uc2 = (unsigned char *) s2;
+	uc1 = (unsigned char *)s1;
+	uc2 = (unsigned char *)s2;
 	i = 0;
 	while ((uc1[i] || uc2[i]) && i < n)
 	{
 		if (uc1[i] != uc2[i])
 			return (uc1[i] - uc2[i]);
-		i ++;
+		i++;
 	}
 	return (0);
 }
